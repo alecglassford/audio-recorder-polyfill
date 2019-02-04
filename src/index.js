f25aa84517d369d691eb132e6f8af523083266fd
Pasquale Mangialavori <pasquale.mangialavori@gmail.com>
2019-02-04 12:03:07 +0100

    converted to es6

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import createWorker from './createWorker.js';
 import encoder from './wave-encoder.js';
 
-var AudioContext = window.AudioContext || window.webkitAudioContext;
+const AudioContext = window.AudioContext || window.webkitAudioContext;
 
 /**
  * Audio Recorder with MediaRecorder API.
@@ -15,40 +15,33 @@ var AudioContext = window.AudioContext || window.webkitAudioContext;
  *
  * @class
  */
-function MediaRecorder(stream) {
+class MediaRecorder {
   /**
    * The `MediaStream` passed into the constructor.
    * @type {MediaStream}
    */
-  this.stream = stream
-
-  /**
-   * The current state of recording process.
-   * @type {"inactive"|"recording"|"paused"}
-   */
-  this.state = 'inactive'
-
-  this.em = document.createDocumentFragment()
-  this.encoder = createWorker(MediaRecorder.encoder);
-
-  var recorder = this
-  this.encoder.addEventListener('message', function (e) {
-    var event = new Event('dataavailable')
-    event.data = new Blob([e.data], { type: recorder.mimeType })
-    recorder.em.dispatchEvent(event)
-    if (recorder.state === 'inactive') {
-      recorder.em.dispatchEvent(new Event('stop'))
-    }
-  })
-}
-
-MediaRecorder.prototype = {
-  /**
-   * The MIME type that is being used for recording.
-   * @type {string}
-   */
-  mimeType: 'audio/wav',
-
+  constructor(stream) {
+    this.mimeType = 'audio/wav';
+    this.stream = stream;
+    /**
+     * The current state of recording process.
+     * @type {"inactive"|"recording"|"paused"}
+     */
+    this.state = 'inactive'
+  
+    this.em = document.createDocumentFragment()
+    this.encoder = createWorker(MediaRecorder.encoder);
+  
+    const recorder = this
+    this.encoder.addEventListener('message', function (e) {
+      const event = new Event('dataavailable')
+      event.data = new Blob([e.data], { type: recorder.mimeType })
+      recorder.em.dispatchEvent(event)
+      if (recorder.state === 'inactive') {
+        recorder.em.dispatchEvent(new Event('stop'))
+      }
+    });
+  }
   /**
    * Begins recording media.
    *
@@ -60,146 +53,146 @@ MediaRecorder.prototype = {
    *
    * @example
    * recordButton.addEventListener('click', function () {
-   *   recorder.start()
-   * })
-   */
-  start: function start (timeslice) {
-    if (this.state === 'inactive') {
-      this.state = 'recording'
-
-      this.context = new AudioContext()
-      var input = this.context.createMediaStreamSource(this.stream)
-      var processor = this.context.createScriptProcessor(2048, 1, 1)
-
-      var recorder = this
-      processor.onaudioprocess = function (e) {
-        if (recorder.state === 'recording') {
-          recorder.encoder.postMessage([
-            'encode', e.inputBuffer.getChannelData(0)
-          ])
-        }
-      }
-
-      input.connect(processor)
-      processor.connect(this.context.destination)
-
-      this.em.dispatchEvent(new Event('start'))
-
-      if (timeslice) {
-        this.slicing = setInterval(function () {
-          if (recorder.state === 'recording') recorder.requestData()
-        }, timeslice)
-      }
-    }
-  },
-
-  /**
-   * Stop media capture and raise `dataavailable` event with recorded data.
-   *
-   * @return {undefined}
-   *
-   * @example
-   * finishButton.addEventListener('click', function () {
-   *   recorder.stop()
-   * })
-   */
-  stop: function stop () {
-    if (this.state !== 'inactive') {
-      this.requestData()
-      this.state = 'inactive'
-      clearInterval(this.slicing)
-    }
-  },
-
-  /**
-   * Pauses recording of media streams.
-   *
-   * @return {undefined}
-   *
-   * @example
-   * pauseButton.addEventListener('click', function () {
-   *   recorder.pause()
-   * })
-   */
-  pause: function pause () {
-    if (this.state === 'recording') {
-      this.state = 'paused'
-      this.em.dispatchEvent(new Event('pause'))
-    }
-  },
-
-  /**
-   * Resumes media recording when it has been previously paused.
-   *
-   * @return {undefined}
-   *
-   * @example
-   * resumeButton.addEventListener('click', function () {
-   *   recorder.resume()
-   * })
-   */
-  resume: function resume () {
-    if (this.state === 'paused') {
-      this.state = 'recording'
-      this.em.dispatchEvent(new Event('resume'))
-    }
-  },
-
-  /**
-   * Raise a `dataavailable` event containing the captured media.
-   *
-   * @return {undefined}
-   *
-   * @example
-   * this.on('nextData', function () {
-   *   recorder.requestData()
-   * })
-   */
-  requestData: function requestData () {
-    if (this.state !== 'inactive') {
-      this.encoder.postMessage(['dump', this.context.sampleRate])
-    }
-  },
-
-  /**
-   * Add listener for specified event type.
-   *
-   * @param {"start"|"stop"|"pause"|"resume"|"dataavailable"} type Event type.
-   * @param {function} listener The listener function.
-   *
-   * @return {undefined}
-   *
-   * @example
-   * recorder.addEventListener('dataavailable', function (e) {
-   *   audio.src = URL.createObjectURL(e.data)
-   * })
-   */
-  addEventListener: function addEventListener () {
-    this.em.addEventListener.apply(this.em, arguments)
-  },
-
-  /**
-   * Remove event listener.
-   *
-   * @param {"start"|"stop"|"pause"|"resume"|"dataavailable"} type Event type.
-   * @param {function} listener The same function used in `addEventListener`.
-   *
-   * @return {undefined}
-   */
-  removeEventListener: function removeEventListener () {
-    this.em.removeEventListener.apply(this.em, arguments)
-  },
-
-  /**
-   * Calls each of the listeners registered for a given event.
-   *
-   * @param {Event} event The event object.
-   *
-   * @return {boolean} Is event was no canceled by any listener.
-   */
-  dispatchEvent: function dispatchEvent () {
-    this.em.dispatchEvent.apply(this.em, arguments)
-  }
+    *   recorder.start()
+    * })
+    */
+   start(timeslice) {
+     if (this.state === 'inactive') {
+       this.state = 'recording'
+ 
+       this.context = new AudioContext()
+       var input = this.context.createMediaStreamSource(this.stream)
+       var processor = this.context.createScriptProcessor(2048, 1, 1)
+ 
+       var recorder = this
+       processor.onaudioprocess = function (e) {
+         if (recorder.state === 'recording') {
+           recorder.encoder.postMessage([
+             'encode', e.inputBuffer.getChannelData(0)
+           ])
+         }
+       }
+ 
+       input.connect(processor)
+       processor.connect(this.context.destination)
+ 
+       this.em.dispatchEvent(new Event('start'))
+ 
+       if (timeslice) {
+         this.slicing = setInterval(function () {
+           if (recorder.state === 'recording') recorder.requestData()
+         }, timeslice)
+       }
+     }
+   }
+ 
+   /**
+    * Stop media capture and raise `dataavailable` event with recorded data.
+    *
+    * @return {undefined}
+    *
+    * @example
+    * finishButton.addEventListener('click', function () {
+    *   recorder.stop()
+    * })
+    */
+   stop() {
+     if (this.state !== 'inactive') {
+       this.requestData()
+       this.state = 'inactive'
+       clearInterval(this.slicing)
+     }
+   }
+ 
+   /**
+    * Pauses recording of media streams.
+    *
+    * @return {undefined}
+    *
+    * @example
+    * pauseButton.addEventListener('click', function () {
+    *   recorder.pause()
+    * })
+    */
+   pause() {
+     if (this.state === 'recording') {
+       this.state = 'paused'
+       this.em.dispatchEvent(new Event('pause'))
+     }
+   }
+ 
+   /**
+    * Resumes media recording when it has been previously paused.
+    *
+    * @return {undefined}
+    *
+    * @example
+    * resumeButton.addEventListener('click', function () {
+    *   recorder.resume()
+    * })
+    */
+   resume() {
+     if (this.state === 'paused') {
+       this.state = 'recording'
+       this.em.dispatchEvent(new Event('resume'))
+     }
+   }
+ 
+   /**
+    * Raise a `dataavailable` event containing the captured media.
+    *
+    * @return {undefined}
+    *
+    * @example
+    * this.on('nextData', function () {
+    *   recorder.requestData()
+    * })
+    */
+   requestData() {
+     if (this.state !== 'inactive') {
+       this.encoder.postMessage(['dump', this.context.sampleRate])
+     }
+   }
+ 
+   /**
+    * Add listener for specified event type.
+    *
+    * @param {"start"|"stop"|"pause"|"resume"|"dataavailable"} type Event type.
+    * @param {function} listener The listener function.
+    *
+    * @return {undefined}
+    *
+    * @example
+    * recorder.addEventListener('dataavailable', function (e) {
+    *   audio.src = URL.createObjectURL(e.data)
+    * })
+    */
+   addEventListener() {
+     this.em.addEventListener.apply(this.em, arguments)
+   }
+ 
+   /**
+    * Remove event listener.
+    *
+    * @param {"start"|"stop"|"pause"|"resume"|"dataavailable"} type Event type.
+    * @param {function} listener The same function used in `addEventListener`.
+    *
+    * @return {undefined}
+    */
+   removeEventListener() {
+     this.em.removeEventListener.apply(this.em, arguments)
+   }
+ 
+   /**
+    * Calls each of the listeners registered for a given event.
+    *
+    * @param {Event} event The event object.
+    *
+    * @return {boolean} Is event was no canceled by any listener.
+    */
+   dispatchEvent() {
+     this.em.dispatchEvent.apply(this.em, arguments)
+   }
 }
 
 /**
